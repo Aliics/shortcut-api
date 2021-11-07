ee32fccf133614c34b01b94d2da53c5fe7f155dc
Alexander Johnston <Aliics@hotmail.com>
2021-11-08 11:41:35 +1300

    separate branch name logic

--- cmd/shortcut-cli/main.go
+++ cmd/shortcut-cli/main.go
@@ -1,38 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/aliics/shortcut-api"
-	"net/url"
 	"os"
-	"strconv"
-	"strings"
 )
 
 var (
-	mostRecentFlag = flag.String("most-recent", "", "Most recent story for a user.")
+	mostRecent string
+	branchName bool
 )
 
 func main() {
+	flag.StringVar(&mostRecent, "most-recent", "", "Most recent story for a user.")
+	flag.BoolVar(&branchName, "branch-name", false, "Show stories as branch names.")
+	flag.Parse()
+
 	api := shortcut_api.NewShortcut(
 		shortcut_api.WithShortcutToken(os.Getenv("SHORTCUT_TOKEN")),
 		shortcut_api.WithUrl("https://api.app.shortcut.com/api/v3"),
 	)
 
-	flag.Parse()
+	if mostRecent != "" {
+		handleMostRecent(api)
+	}
+}
 
-	if *mostRecentFlag != "" {
-		stories, err := api.SearchStories("owner:"+*mostRecentFlag, 1)
+func handleMostRecent(api *shortcut_api.Shortcut) {
+	stories, err := api.SearchStories("owner:"+mostRecent, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	story := stories.Data[0]
+
+	if branchName {
+		fmt.Println(story.BranchName(mostRecent))
+	} else {
+		output, err := json.Marshal(story)
 		if err != nil {
 			panic(err)
 		}
-
-		story := stories.Data[0]
-		owner := strings.ReplaceAll(strings.ToLower(url.PathEscape(*mostRecentFlag)), "%20", "-")
-		id := strconv.Itoa(story.Id)
-		name := strings.ReplaceAll(strings.ToLower(url.PathEscape(story.Name)), "%20", "-")
-
-		fmt.Println(owner + "/sc-" + id + "/" + name)
+		fmt.Println(string(output))
 	}
 }
